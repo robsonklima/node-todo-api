c0425c16466c5a21517f027132a0381b3f05b0ac
robsonklima <robsonklima@gmail.com>
2017-05-26 19:34:06 -0300

    Mongo Mongoose-queries

--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    completed: req.body.completed,
+    completedAt: req.body.completedAt
   })
 
   todo.save().then((doc) => {
@@ -21,6 +23,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
