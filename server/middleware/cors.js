37659cb8efc85e25a23643303df0683b53345a64
robsonklima <robsonklima@gmail.com>
2017-06-01 22:36:42 -0300

    teste commit apos alteracao de senha

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,4 +1,3 @@
-// midleware
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
