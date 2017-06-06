291f01dd30c53ff45aa3354ce3f0531c875e114b
robsonklima <robsonklima@gmail.com>
2017-06-06 20:09:18 -0300

    Add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -105,6 +105,16 @@ UserSchema.pre('save', function (next) {
   }
 });
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
